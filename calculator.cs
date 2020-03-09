2ef8af2afd5a6c75cbddf5c5aefbc2b8abed95a4
hilmanauliarahman <hilman.ra@students.amikom.ac.id>
2020-03-09 17:03:00 +0700

    langkah 2 fungsi

--- calculator.cs
+++ calculator.cs
@@ -1,9 +1,15 @@
-//19.11.2656
 class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
 
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
 	}
 
 	static int Penambahan(int a, int b)
@@ -11,6 +17,9 @@ class Calculator
 		return a + b;
 	}
 
-	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}	
 }
 
